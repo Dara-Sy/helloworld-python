0b774ab39871134d335f4295cc149c6b0f54886e
Dara-Sy <darajanne@gmail.com>
2018-06-26 16:45:22 -0400

    adding .py file

---
+++ hello.py
@@ -0,0 +1,17 @@
+print("hello world")
+
+b = "dara is learning python"
+
+print(b)
+
+# swapping variables in python
+
+v1 = "first string"
+v2 = "second string"
+
+temp = v1
+# temp = first string
+v1 = v2
+# v1 = second string
+v2 = temp
+# v2 = first string
